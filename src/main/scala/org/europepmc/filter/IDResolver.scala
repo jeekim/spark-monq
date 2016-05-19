d9e136974f5a1f71d2c40ed13cd26326c1b7d840
Jee-Hyub Kim <linkedcarbon@gmail.com>
2016-05-19 16:31:37 +0100

    remove lib and src

--- src/main/scala/org/europepmc/filter/IDResolver.scala
+++
@@ -1,5 +0,0 @@
-package org.europepmc.filter;
-
-trait IDResolver {
-   def isValidID(domain: String, id: String): Boolean 
-}
--- src/main/scala/org/europepmc/filter/QueryGenerator.scala
+++
@@ -1,9 +0,0 @@
-package org.europepmc.filter;
-
-// TODO use pattern matching to generate queries depending on db types.
-
-case class QueryGenerator(q: String) {
-
-  override def toString() = s"query: $q"
-
-}

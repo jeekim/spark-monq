4b0964cd52d46ac87a4628cd289a7a3c12f60eb0
Jee-Hyub Kim <linkedcarbon@gmail.com>
2016-06-01 09:38:01 +0100

    ADTs for Named Entities

--- core/src/main/scala/org/europepmc/pipeline/Pipeline.scala
+++ core/src/main/scala/org/europepmc/pipeline/Pipeline.scala
@@ -1,26 +1,24 @@
 package org.europepmc.pipeline
 
 trait Pipeline {
-
-  type Tagger
-  type Filter
-
   type Article
   type Section
-  type T
-  type Sentence[T]
+  type Sentence[Mention]
 
   type Mention
-  type Entity
+  /* type Entity
 
   type Gene <: Entity
   type Species
   type Chemical
   type Disease
-  type GO
+  type GO */
 
-  // type Tagging = Sentence[T] => Sentence[T]
-  type Tagging = String => Annotation
+  // type Tagger
+  // type Filter
+
+  type Tagger = String => Mention
+  type Filter = Mention => NamedEntity
 }
 
 trait Tagger {
@@ -29,4 +27,16 @@ trait Tagger {
 
 trait Annotation {
   def exsits: Boolean
+  def get: NamedEntity
 }
+
+sealed trait NamedEntity
+case class Gene(id: String, name: String) extends NamedEntity
+case class Species(id: String, name: String) extends NamedEntity
+case class GO(id: String, name: String) extends NamedEntity
+case class Disease(id: String, name: String) extends NamedEntity
+case class Chemical(id: String, name: String) extends NamedEntity
+case class AccNumber(id: String, name: String) extends NamedEntity
+
+object RunTagger
+object RunFilter

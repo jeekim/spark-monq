399cc84d77858aa078fe499a6ab9852e09753bb0
Jee-Hyub Kim <linkedcarbon@gmail.com>
2016-06-14 14:28:51 +0100

    refine the algebra for Text-Mining pipeline API

--- core/src/main/scala/org/europepmc/pipeline/Pipeline.scala
+++ core/src/main/scala/org/europepmc/pipeline/Pipeline.scala
@@ -1,7 +1,8 @@
 package org.europepmc.pipeline
 
+/* Pipeline */
 trait Pipeline {
-
+  // ADT
   sealed trait Status
   case object Raw extends Status
   case object Tagged extends Status
@@ -12,54 +13,75 @@ trait Pipeline {
   // Data source
   // Patent, Wikipage, MEDLINE abstract, etc.
   type Collection[Article]
-  type Article[Section]
-  // type Section[Sentence]
-  // type Collection = Seq[Article]
-  // type Article = Seq[Section]
-  type Section = Seq[Sentence]
 
+  type Section = Seq[Sentence]
   type Title <: Section
   type Abstract <: Section
   type Introduction <: Section
   type Method <: Section
   type Result <: Section
   type Discussion <: Section
-
   type Sentence
-
-  // Annotation
-  type AnnotatedSentence
-  type Mention
-  type Relation
-  type Event
-  type Tagger
-  type Filter
-  // object Filter
-
-  def compose: Tagger => Tagger => Tagger
 }
 
-object Pipeline {
-  def buildTagger: Tagger = ???
-  def buildFilter: Filter = ???
-  def buildPipeline: Pipeline = ???
+trait PipelineService {
+  def compose: Tagger => Filter => Tagger
+  def buildPipeline: Pipeline
 }
 
-trait DictionaryService
+object Pipeline
 
-trait TaggingService
+/* Article */
+case class Article (xml: String) {
+  type Section = String
+  type Sentence = String
 
-trait FilteringService
+  def split: Seq[Section] = ???
+}
 
-trait CollectionReader
+trait TaggedArticle
+trait ArticleRepository
 
-// Type class for Mention?
-trait Filterable[Mention[_]]
+/* Dictionary */
+trait Dictionary
+trait DictionaryRepository
 
+trait DictionaryService {
+  def generate: String => Dictionary
+}
+
+/* Tagger */
 trait Tagger {
   type Text
+  type Dictionary
   def transform: Text => Text
 }
+
+trait TaggingService {
+  type Text
+  type TaggedText
+
+  def buildTagger: Dictionary => Tagger
+  def runTagger[A <: Text]: Tagger => Text => TaggedText
+}
+
+// trait SectionTagger[Article] extends Tagger {
+trait SectionTagger extends Tagger {
+  // def transform(in: String): String = in
+  type Text = String
+  def transform = s => "<" + s + ">"
+  def run: Tagger = ???
+}
+
+object testTaggingService extends TaggingService {
+  type Text = String
+  type TaggedText = String
+  def buildTagger = ???
+  def runTagger[Article] = ???
+}
+
+/* Filter */
+trait Blacklist
 
 trait Filter {
   type Text[A]
@@ -67,38 +89,51 @@ trait Filter {
   def transform: Text[Mention] => Text[Option[Mention]]
 }
 
-// trait Filter with ValidationRuleSet
+trait Filterable[Mention[_]]
 
+trait FilteringService {
+  def buildFilter: Blacklist => Filter
+}
+
+/* Annotation */
 trait Annotation {
+  type AnnotatedSentence
+  type Mention
+  type Relation
+  type Event
+
   def exsits: Boolean
   def uri: Option[String]
   def get: NamedEntity
+
+  sealed trait NamedEntity {
+    def id: String
+    def name: String
+  }
+
+  case class Gene(id: String, name: String) extends NamedEntity
+  case class Species(id: String, name: String) extends NamedEntity
+  case class GO(id: String, name: String) extends NamedEntity
+  case class Disease(id: String, name: String) extends NamedEntity
+  case class Chemical(id: String, name: String) extends NamedEntity
+  case class AccNumber(id: String, name: String) extends NamedEntity
 }
 
-sealed trait NamedEntity {
-  def id: String
-  def name: String
-}
+trait AnnotationRepository
+trait AnnotationService
 
-// type Gene <: NamedEntity
-case class Gene(id: String, name: String) extends NamedEntity
-case class Species(id: String, name: String) extends NamedEntity
-case class GO(id: String, name: String) extends NamedEntity
-case class Disease(id: String, name: String) extends NamedEntity
-case class Chemical(id: String, name: String) extends NamedEntity
-case class AccNumber(id: String, name: String) extends NamedEntity
-
-// trait SectionTagger[Article] extends Tagger {
-trait SectionTagger extends Tagger {
-  // def transform(in: String): String = in
-  type Text = String
-  def transform = s => s + "tagged"
-}
 
 // object sectionTagger extends Tagger with SectionTagger
 object sectionTagger extends SectionTagger
 object RunTagger
 object RunFilter
+
+/* Others */
+trait TextAnalyticsService
+trait ReportGenerationService
+trait RdfGenerationService
+trait CollectionReader
+
 
 package application {
 
